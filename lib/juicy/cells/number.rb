bc6a4f2668f6cf70a620ee9a9b33af5d5434313a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2013-09-09 10:14:09 -0300

    Number cell value is converted to float

--- lib/juicy/cells/number.rb
+++ lib/juicy/cells/number.rb
@@ -2,6 +2,10 @@ module Juicy
   module Cells
     class Number < Base
       include Juicy::Cells::Extensions::Number
+
+      def value
+        super.to_f
+      end
     end
   end
 end
--- spec/lib/juicy/cells/number_spec.rb
+++ spec/lib/juicy/cells/number_spec.rb
@@ -9,4 +9,9 @@ describe Juicy::Cells::Number do
   it "inherits from Juicy::Cells::Extensions::Number" do
     expect(described_class.ancestors).to include(Juicy::Cells::Extensions::Number)
   end
+
+  it "converts value to float" do
+    subject = described_class.new(double(:report), '1')
+    expect(subject.value).to eq('1'.to_f)
+  end
 end
